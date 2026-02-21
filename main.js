a65c983f56ab0277fb0ad3d5054c510b5c9a39f1
luisDaniel <estudiosluisperez@gmail.com>
2026-02-21 01:07:39 -0500

    feat se  creo las funciones que muestra la informaicon del clima menos la imagen , la imagen no la encuentra

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-import { ObtenerClima } from "./obtenerClima.js";
-
+import { ObtenerClima, ObtenerImagen } from "./obtenerClima.js";
 
 const searchINPUT = document.querySelector("#ciudad-input");
 const searchBTN = document.querySelector("#buscar-btn");
-const errorMensaje = document.querySelector("#error-mensaje");
-const climaIcono = document.querySelector("#clima-icono");
 
-searchBTN.addEventListener("click" , () => {
-    ObtenerClima(searchINPUT.value)
+
+searchBTN.addEventListener("click", initAPP);
+searchINPUT.addEventListener("keydown" , (even) => {
+    if (even.key === "Enter") {
+        initAPP()
+    }
 })
+
+function initAPP() {
+    // envio todos los datos del clima
+    ObtenerClima(searchINPUT.value).then((clima) => {
+        document.querySelector("#temperatura").textContent = `${Math.round(clima.main.temp)} -°C`;
+        document.querySelector("#ciudad-nombre").textContent = clima.name;
+        document.querySelector("#humedad").textContent = `${clima.main.humidity} -%`;
+        document.querySelector("#viento").textContent = `${clima.wind.speed} -km/h`;
+    })
+    // info de la imagen usando la misma api
+    let urlImagen = ObtenerImagen();
+    document.querySelector("#clima-icono").src = urlImagen;
+}
--- obtenerClima.js
+++ obtenerClima.js
@@ -1,12 +1,27 @@
-export function ObtenerClima(city) {
+export async function ObtenerClima(city) {
     const apiKey = `625da1110932688dd5055a71d021ca41`;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    fetch(apiUrl)
-        .then((d) => d.json())
-        .then((dato) => {
-            let datos = dato;
-            console.log(datos);
-        })
-        .catch((e) => console.error(e));
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        ObtenerImagen(data.weather[0].icon);
+
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export function ObtenerImagen(data) {
+    try {
+        const iconCodigo = data;
+        console.log(iconCodigo);
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCodigo}@2x.png`;
+
+        return iconUrl;
+    } catch (error) {
+        console.error(error);
+    }
 }
