a0d9cbb7f0a52f13718b71470aa15612e4eb061d
luisDaniel <estudiosluisperez@gmail.com>
2026-02-21 23:16:50 -0500

    feat se creo la funcionalidad del contador de fotos cargadas en pantalla

--- galeryPhotos/galery.js
+++ galeryPhotos/galery.js
@@ -1,5 +1,7 @@
 const galeria = document.querySelector("#galeria");
-const btn = document.querySelector("button");
+const btnCargarMas = document.querySelector("#cargar-mas-btn");
+const btnLimpiar = document.querySelector("#limpiar-btn");
+const fotosCount = document.querySelector("#fotos-count");
 
 function odtenerDatos() {
     const url = "https://jsonplaceholder.typicode.com/photos";
@@ -7,8 +9,9 @@ function odtenerDatos() {
     fetch(url)
         .then((dato) => dato.json())
         .then((fotos) => {
-            const primerasDiez = fotos.slice(0 , 10)
-            primerasDiez.forEach(foto => {
+            const primerasDiez = fotos.slice(0, 10);
+            ContadorFotosLocalStorage(primerasDiez.length);
+            primerasDiez.forEach((foto) => {
                 galeria.innerHTML += `
                     <div class="card" style="width: 18rem;">
                         <img src="${foto.url}" class="card-img-top" alt="...">
@@ -17,13 +20,45 @@ function odtenerDatos() {
                         </div>
                     </div>
                 `;
-            })
+            });
         })
         .catch((e) => {
             console.error(e);
         });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+function ContadorFotosLocalStorage(cantidad) {
+    let cantidadFotos = JSON.parse(localStorage.getItem("cantidadFotos")) || [];
+    cantidadFotos.push(cantidad);
+
+    localStorage.setItem("cantidadFotos", JSON.stringify(cantidadFotos));
+}
+
+function TotalCantidadFotos() {
+    let cantidadFotos = JSON.parse(localStorage.getItem("cantidadFotos"));
+
+    let sumaFotos = cantidadFotos.reduce((total, fotos) => total + fotos, 0);
+
+    return sumaFotos;
+}
+
+function Limpiar() {
+    galeria.innerHTML = "";
+    let cantidadFotos = JSON.parse(localStorage.getItem("cantidadFotos"));
+
+    cantidadFotos = localStorage.clear();
+
+    fotosCount.innerHTML = "0"
+}
+
+btnCargarMas.addEventListener("click", () => {
     odtenerDatos();
+    setTimeout(() => {
+        let totalFotos = TotalCantidadFotos();
+        fotosCount.innerHTML = totalFotos;
+    }, 100);
+});
+
+btnLimpiar.addEventListener("click", () => {
+    Limpiar();
 });
