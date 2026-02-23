487da6d386242833ee0381db6a997d402054f2d1
luisDaniel <estudiosluisperez@gmail.com>
2026-02-23 01:40:15 -0500

    feat se coloco a funcionar la api mostrando todos los 150 pokemon en lapantalla , tambien se le creo un iu basico solo para mostrarlo con css y html

--- pokeapi/pokeapi.js
+++ pokeapi/pokeapi.js
@@ -1,8 +1,48 @@
+let body = document.querySelector("#body");
 const urlApi = `https://pokeapi.co/api/v2/pokemon/`;
 
 for (let index = 1; index < 151; index++) {
     fetch(urlApi + index)
         .then((response) => response.json())
-        .then((data) => data)
+        .then((poke) => {
+            
+                body.innerHTML += `
+                    <div class="card" id="pokemon-card">
+                        <p class="card-id">#${poke.id}</p>
+
+                        <div class="card-sprite">
+                            <!-- Con imagen: reemplaza el div por <img src="url_sprite" alt="nombre" /> -->
+                            <img  src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}"/>
+                            
+                        </div>
+
+                        <p class="card-name">— ${poke.name} —</p>
+
+                        <div class="card-types">
+                            <span class="type">tipo</span>
+                        </div>
+
+                        <div class="card-stats">
+                            <div class="stat">
+                                <p class="stat-label">Altura</p>
+                                <p class="stat-value">${poke.height}</p>
+                            </div>
+                            <div class="stat">
+                                <p class="stat-label">Peso</p>
+                                <p class="stat-value">${poke.weight}</p>
+                            </div>
+                            <div class="stat">
+                                <p class="stat-label">HP</p>
+                                <p class="stat-value">—</p>
+                            </div>
+                            <div class="stat">
+                                <p class="stat-label">Exp. base</p>
+                                <p class="stat-value">—</p>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            
+        })
         .catch((e) => console.error(e));
 }
